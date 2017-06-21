b9f58d7df4fc9893632ec3c801f496e75b86fc74
sluckyboy <syajunhome@163.com>
2017-06-21 22:37:48 +0800

    add array

---
+++ register.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>用户注册页面</title>
+</head>
+<body>
+    <form method="post" action="./register.php">
+        
+    </form>
+</body>
+</html>
